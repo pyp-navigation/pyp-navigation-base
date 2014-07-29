8d2397151a3187827092d56b2bb266538b4c40e0
zhangweijie <531724220@qq.com>
2014-07-29 15:11:40 +0800

    测试

--- pyp-navigation-base/src/pyp/navigation/map/MapFragment.java
+++ pyp-navigation-base/src/pyp/navigation/map/MapFragment.java
@@ -37,7 +37,7 @@ import com.qozix.tileview.TileView;
  * @email 531724220@qq.com
  */
 public class MapFragment extends Fragment {
-	//test 
+	//test
 	private MainActivity parentActivity;
 	
 	@Override
